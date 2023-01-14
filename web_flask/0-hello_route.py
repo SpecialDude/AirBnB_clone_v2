f877fd0f204b96be66c7498a14e9b15388dbaf6b
SpecialDude <warithadetayo.awa@gmail.com>
2023-01-15 00:10:20 +0100

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,19 +9,14 @@ With a single routing
 import flask
 
 
-def create_flask_app():
-    """Creates the flask application"""
+app = flask.Flask(__name__)
 
-    app = flask.Flask(__name__)
+@app.route("/", strict_slashes=False)
+def home():
+    """ The home page """
 
-    @app.route("/", strict_slashes=False)
-    def home():
-        """ The home page """
-
-        return "Hello HBNB!"
-
-    return app
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    create_flask_app().run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
