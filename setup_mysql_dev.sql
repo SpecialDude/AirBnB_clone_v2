b4f1db8c930e9ed5daefd1da0c7fc3a784f51192
SpecialDude <warithadetayo.awa@gmail.com>
2023-01-15 13:13:35 +0100

    MySQL Prep

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,5 @@
+# This Setups a the database for hbnb
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
